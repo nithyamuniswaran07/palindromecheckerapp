36e71a05acb044b9f0e4cc749bcd4e7a3947b025
nithyamuniswaran07 <nithees353@gmail.com>
2026-03-17 00:39:34 +0530

    UC7: Deque based optimized palindrome checker

---
+++ PalindromeCheckerApp/src/UseCase7DequeBased.java
@@ -0,0 +1,25 @@
+﻿import java.util.Scanner;
+import java.util.ArrayDeque;
+import java.util.Deque;
+public class UseCase7DequeBased {
+    public static void main(String[] args) {
+        System.out.println("============================================================");
+        System.out.println("         Welcome to Palindrome Checker App");
+        System.out.println("============================================================");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word or sentence to check: ");
+        String input = scanner.nextLine();
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : input.toCharArray()) deque.add(c);
+        boolean isPalindrome = true;
+        while (deque.size() > 1) {
+            if (!deque.pollFirst().equals(deque.pollLast())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input          : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        scanner.close();
+    }
+}
---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+
+            String input = "noon";
+
+
+            Stack<Character> stack = new Stack<>();
+
+
+            for (char c : input.toCharArray()) {
+                stack.push(c);
+            }
+
+
+            boolean isPalindrome = true;
+
+            for (char c : input.toCharArray()) {
+                if (c != stack.pop()) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+
+            if (isPalindrome) {
+                System.out.println(input + " is a Palindrome");
+            } else {
+                System.out.println(input + " is not a Palindrome");
+            }
+        }
+    }
