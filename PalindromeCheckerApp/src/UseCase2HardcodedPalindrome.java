e6464d2634b3742416492229888ede11448732c2
nithyamuniswaran07 <nithees353@gmail.com>
2026-03-17 00:39:15 +0530

    UC2: Hardcoded palindrome check

---
+++ PalindromeCheckerApp/src/UseCase2HardcodedPalindrome.java
@@ -0,0 +1,13 @@
+﻿public class UseCase2HardcodedPalindrome {
+    public static void main(String[] args) {
+        System.out.println("============================================================");
+        System.out.println("         Welcome to Palindrome Checker App");
+        System.out.println("============================================================");
+        String input = "level";
+        String reversed = new StringBuilder(input).reverse().toString();
+        boolean isPalindrome = input.equals(reversed);
+        System.out.println("Input          : " + input);
+        System.out.println("Reversed       : " + reversed);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
