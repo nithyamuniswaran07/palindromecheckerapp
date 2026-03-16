28762fa6be878c058fd058ee1923e39f81492be5
nithyamuniswaran07 <nithees353@gmail.com>
2026-03-17 00:39:09 +0530

    UC1: Add welcome message module

---
+++ PalindromeCheckerApp/src/UseCase1WelcomeMessage.java
@@ -0,0 +1,10 @@
+﻿public class UseCase1WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("============================================================");
+        System.out.println("         Welcome to Palindrome Checker App");
+        System.out.println("============================================================");
+        System.out.println("This application checks whether a given word or");
+        System.out.println("sentence is a Palindrome.");
+        System.out.println("============================================================");
+    }
+}
